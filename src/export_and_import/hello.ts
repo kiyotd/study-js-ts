5ce1b1267c69001b505ced6327f80bc95a1d306e
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-08-21 11:13:17 +0900

    How to use export/import

---
+++ src/export_and_import/hello.ts
@@ -0,0 +1,6 @@
+export const num1 = 1;
+export const num2 = 2;
+
+// export default は1回だけできる
+const hello = "hello";
+export default hello;
---
+++ src/export_and_import/index.ts
@@ -0,0 +1,7 @@
+import hello, {num1, num2} from "./hello";
+
+// 3
+console.log(num1 + num2);
+
+// hello
+console.log(hello);
