badae2d5cc66a6a404a89211b4c30543dd043111
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-08-22 19:52:41 +0900

    Add 同期処理と非同期処理

---
+++ src/js/非同期処理/2_同期処理と非同期処理/main.js
@@ -0,0 +1,21 @@
+function sleep(ms) {
+  const startTime = new Date();
+  while (new Date() - startTime < ms) {
+    //
+  }
+
+  console.log("sleep done");
+}
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  console.log("button clicked");
+});
+
+setTimeout(() => {
+  sleep(3000);
+}, 2000);
+
+// sleep(3000);
+// debugger;
