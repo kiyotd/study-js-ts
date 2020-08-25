9341a5e16ac9a9b6a3930d7ef394f39ee4bc3001
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-08-25 19:53:40 +0900

    Add ts : Intersection types

---
+++ src/ts/intersection-types/1.ts
@@ -0,0 +1,21 @@
+type Engineer = {
+  name: string;
+  role: string;
+};
+type Blogger = {
+  name: string;
+  follower: number;
+};
+type EngineerBlogger = Engineer & Blogger;
+
+const quill: EngineerBlogger = {
+  name: "quill",
+  role: "front-end",
+  follower: 1000
+};
+
+type tmp = string & number; //never型
+
+type NumberBoolean = number | boolean;
+type StringNumber = string | number;
+type Mix = NumberBoolean & StringNumber; //number型
