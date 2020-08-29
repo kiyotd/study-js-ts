2492eea9f0607f5e90401f26501bf3cc26886a64
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-08-29 16:57:37 +0900

    Add comment

--- src/js/export-import/sample-1/index.mjs
+++ src/js/export-import/sample-1/index.mjs
@@ -1,8 +1,12 @@
 // package.json で "type": "module"
 // とすると js ファイルがES Modules として扱われるため
 // 拡張子が mjs でなくてもよい
-// import は拡張子が必要
 // 省略したい場合は --es-module-specifier-resolution=node
+//
+// ES Modules の import には拡張子が必要
+//
+// html から <script type="module" src=""></script>
+// として読み込むことも可能
 
 import { hello } from "./lib.mjs";
 
