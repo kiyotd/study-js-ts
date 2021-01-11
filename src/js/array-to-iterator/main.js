502293fe2e0c1555776281dfa9ceb65098a6b3e0
kiyotd <torigoedesign+kiyotd@gmail.com>
2021-01-11 14:04:47 +0900

    feat(js): 配列をイテレーターに変換

---
+++ src/js/array-to-iterator/main.js
@@ -0,0 +1,9 @@
+const arr = [0, 1, 2, 3];
+
+const iterator = arr.values();
+
+console.log(iterator.next()); // { value: 0, done: false }
+console.log(iterator.next()); // { value: 1, done: false }
+console.log(iterator.next()); // { value: 2, done: false }
+console.log(iterator.next()); // { value: 3, done: false }
+console.log(iterator.next()); // { value: 0, done: true }
